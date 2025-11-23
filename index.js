401d2b41795459f89c539f19e9d0a423da8d032e
alanhabib <dalanhabib@gmail.com>
2025-11-23 11:37:59 +0100

    playbackservice funtion

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import 'expo-router/entry';
-import TrackPlayer from 'react-native-track-player';
-import { PlaybackService } from './src/services/audio/trackPlayerSetup';
+import "expo-router/entry";
+import TrackPlayer from "react-native-track-player";
+import { PlaybackService } from "./src/services/audio/trackPlayerSetup";
 
 // Register the playback service for background audio
-TrackPlayer.registerPlaybackService(() => PlaybackService);
+TrackPlayer.registerPlaybackService(() => PlaybackService());
